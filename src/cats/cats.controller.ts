184916c717d1fa13918fc5fed623eaa483bfe8fd
shinsw627 <shinsw627@naver.com>
2022-01-13 00:35:10 +0900

    implement success interceptor

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -8,16 +8,20 @@ import {
   Patch,
   Put,
   UseFilters,
+  UseInterceptors,
 } from '@nestjs/common';
-import { HttpExceptionFilter } from 'src/http-exception.filter';
+import { PositiveIntPipe } from 'src/common/pipes/positiveInt.pipe';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 import { CatsService } from './cats.service';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
 
 @Controller('cats')
+@UseInterceptors(SuccessInterceptor)
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
-  @UseFilters(HttpExceptionFilter)
   getAllCat() {
     if ('') {
       throw new HttpException('Unauthorized', 401);
@@ -25,14 +29,15 @@ export class CatsController {
 
     return 'all cat';
   }
+
   //ParseIntPipe 가 타입을 넘버로 바꾼다.
   @Get(':id')
-  getOneCat(@Param('id', ParseIntPipe) param) {
+  getOneCat(@Param('id', ParseIntPipe, PositiveIntPipe) param) {
     console.log(param);
     return 'one cat';
   }
 
-  @Put()
+  @Put(':id')
   updateCat() {
     return 'update cat';
   }
